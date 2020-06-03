e1ba2a58bad544221841649089b12aa8ae957278
Noah Klayman <noahklayman@gmail.com>
2020-06-03 16:20:18 -0700

    chore(package): upgrade deps

--- src/background.js
+++ src/background.js
@@ -12,10 +12,14 @@ const isDevelopment = process.env.NODE_ENV !== 'production'
 let win
 let secondWin
 // Standard scheme must be registered before the app is ready
-protocol.registerStandardSchemes(['app'], { secure: true })
+protocol.registerSchemesAsPrivileged([{scheme: 'app', privileges: { secure: true, standard: true } }])
 function createWindow(winVar, devPath, prodPath) {
   // Create the browser window.
-  winVar = new BrowserWindow({ width: 800, height: 600 })
+  winVar = new BrowserWindow({ width: 800, height: 600, webPreferences: {
+    // Use pluginOptions.nodeIntegration, leave this alone
+    // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
+    nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION
+  } })
 
   if (process.env.WEBPACK_DEV_SERVER_URL) {
     // Load the url of the dev server if in development mode
